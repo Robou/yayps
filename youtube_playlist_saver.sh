e336ef633388f5e3dc49bdad220bfc7dee8d2a9f
Robou <romainbousson@gmail.com>
2017-03-21 00:26:59 +0100

    Add files via upload
    - mis un prompt pour changer de répertoire de sauvegarde si besoin.
    - demande le nom du fichier de sortie
    - demande l'url de la playlist à scanner.

--- youtube_playlist_saver.sh
+++ youtube_playlist_saver.sh
@@ -1,12 +1,42 @@
 #!/bin/sh
 
-echo Répertoire de travail courant : `pwd`
+topdepart="no"
 
-read -p 'Nom du fichier de sortie ?' out
-echo "Fichier de sortie : $out"
+run(){
+	read -p 'Nom du fichier de sortie ?' out
+	echo "Fichier de sortie : $out"
 
-echo "Début de l'analyse..."
-youtube-dl -e --no-warnings -i https://www.youtube.com/playlist?list=PLCF690B0D7B5651D8 | tee -i playlist-$out.backup.txt
+	read -p "adresse de la playlist à recopier ?" playlist_url
+	echo "url de la playlist : $playlist_url"
 
-echo Done.
+	#for testdev purpose:
+		#playlist_url="https://www.youtube.com/playlist?list=PLCF690B0D7B5651D8"
+		#echo "*vraie* url de la playlist : $playlist_url"
 
+	echo "Début de l'analyse..."
+	youtube-dl -e --no-warnings -i $playlist_url | tee -i playlist-$out.backup.txt
+
+	echo Done.
+}
+
+while  [ $topdepart="no" ]; do
+	echo Répertoire de travail courant : `pwd`
+
+	read -p 'continuer ? (O=oui, N=non ou tapper le répertoire voulu)      ' answer
+	
+	case "$answer" in
+		oui | o | O | Oui | OUI ) 
+			topdepart="yes"
+			#echo "answer= $answer"
+			echo "topdepart= $topdepart"
+			run;; 
+    	non | n | N | Non | NON ) 
+			echo "Ok bye bye."
+			break;;
+		* ) 
+			echo "Changement de répertoire..."
+			cd "$answer";;
+	esac
+done
+
+exit 0;
