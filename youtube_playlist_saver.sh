f77f3856c8399d2e3a6c9a14ae77698e572a5b23
Robou <romainbousson@gmail.com>
2017-03-23 19:55:18 +0100

    Add files via upload
    last details added

--- youtube_playlist_saver.sh
+++ youtube_playlist_saver.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+#	YAYPS = Yet Another Youtube Playlist Saver
+#	EN :	Writes every title from a given youtube playlist into a text file for backup purpose.
+#	FR :	Recopie tous les titres d'une playlist Youtube donnée dans un fichier texte pour l'archiver.
+#		
+#	Copyright (C) 2017  Romain Bousson
+
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU Affero General Public License as published
+#	by the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU Affero General Public License for more details.
+
+#	You should have received a copy of the GNU Affero General Public License
+#	along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 topdepart="no"
 
 run(){
@@ -9,27 +29,23 @@ run(){
 	read -p "adresse de la playlist à recopier ?" playlist_url
 	echo "url de la playlist : $playlist_url"
 
-	#for testdev purpose:
-		#playlist_url="https://www.youtube.com/playlist?list=PLCF690B0D7B5651D8"
-		#echo "*vraie* url de la playlist : $playlist_url"
-
 	echo "Début de l'analyse..."
-	youtube-dl -e --no-warnings -i $playlist_url | tee -i playlist-$out.backup.txt
+	youtube-dl -e --no-warnings -i $playlist_url | tee -i BACKUP_Youtube_playlist-$out.txt
 
+	notify-send "YAYPS / Analyse de la playlist terminée !"
 	echo Done.
 }
 
 while  [ $topdepart="no" ]; do
 	echo Répertoire de travail courant : `pwd`
 
-	read -p 'continuer ? (O=oui, N=non ou tapper le répertoire voulu)      ' answer
+	read -p 'continuer ? (O ou ENTER=oui, N=non ou tapper le répertoire voulu si besoin)      ' answer
 	
 	case "$answer" in
-		oui | o | O | Oui | OUI ) 
+		oui | o | O | Oui | OUI |"") 
 			topdepart="yes"
-			#echo "answer= $answer"
-			echo "topdepart= $topdepart"
-			run;; 
+			run
+			break;;
     	non | n | N | Non | NON ) 
 			echo "Ok bye bye."
 			break;;
